43c3b52646d7765ca809426dfc02f41d9efd927c
Michael Berman <143347826+mberma24@users.noreply.github.com>
2025-01-12 17:58:29 -0500

    added creator tabs

--- src/nav/CreatorTabs.js
+++ src/nav/CreatorTabs.js
@@ -1,8 +1,8 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
-import { Text, StyleSheet } from "react-native";
-
+import { Text, StyleSheet, View } from "react-native";
 import SetUp from "../screens/SetUp";
+import Create from "../screens/Create";
 import Manage from "../screens/Manage";
 
 const Tab = createBottomTabNavigator();
@@ -13,38 +13,44 @@ const CreatorTabs = () => {
       screenOptions={({ route }) => ({
         tabBarIcon: ({ color, size, focused }) => {
           let iconName;
-
           switch (route.name) {
-            case "SetUp":
-              iconName = focused ? "calendar" : "calendar-outline";
+            case "Create":
+              iconName = focused ? "alarm" : "alarm-outline";
+              break;
+            case "Setup":
+              iconName = focused ? "add-circle" : "add-circle-outline";
               break;
             case "Manage":
-              iconName = focused ? "people" : "people-outline";
+              iconName = focused ? "person" : "person-outline";
               break;
           }
-          return <Ionicons name={iconName} size={size} color={color} />;
+          return (
+            <View style={focused ? styles.activeIconContainer : styles.inactiveIconContainer}>
+              <Ionicons name={iconName} size={size} color={focused ? "#fff" : "#000"} />
+              <Text
+                style={{
+                  fontSize: 10,
+                  color: focused ? "#fff" : "#000",
+                  fontWeight: focused ? "bold" : "normal",
+                  marginTop: 4
+                }}
+              >
+                {route.name}
+              </Text>
+            </View>
+          );
         },
-        tabBarLabel: ({ color, focused }) => (
-          <Text
-            style={{
-              fontSize: 10,
-              color,
-              fontWeight: focused ? "bold" : "normal",
-            }}
-          >
-            {route.name}
-          </Text>
-        ),
+        tabBarLabel: () => null, // This hides the default label
         tabBarStyle: styles.tabBarStyle,
         tabBarItemStyle: styles.tabBarItemStyle,
         tabBarActiveTintColor: "#000",
-        tabBarInactiveTintColor: "##c4c5c5",
-
+        tabBarInactiveTintColor: "#c4c5c5",
         headerTitleAlign: "center",
-        headerStyle: styles.headerStyle,
+        headerStyle: styles.headerStyle
       })}
     >
-      <Tab.Screen name="SetUp" component={SetUp} />
+      <Tab.Screen name="Create" component={Create} />
+      <Tab.Screen name="Setup" component={SetUp} />
       <Tab.Screen name="Manage" component={Manage} />
     </Tab.Navigator>
   );
@@ -53,7 +59,6 @@ const CreatorTabs = () => {
 const styles = StyleSheet.create({
   tabBarStyle: {
     height: 80,
-
     backgroundColor: "#8bd9fb",
     position: "absolute",
     bottom: 15,
@@ -72,12 +77,31 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     borderRadius: 30,
   },
-
   headerStyle: {
     borderBottomWidth: 0,
     shadowOpacity: 0,
     elevation: 0,
+    backgroundColor: "#fff",
   },
+  activeIconContainer: {
+    backgroundColor: "#000",
+    borderRadius: 50,
+    width: 90,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    verticalAlign: 'middle',
+    marginTop: 15,
+  },
+  inactiveIconContainer: {
+    borderRadius: 50,
+    width: 80,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    verticalAlign: 'middle',
+    marginTop: 10,
+  }
 });
 
 export default CreatorTabs;
